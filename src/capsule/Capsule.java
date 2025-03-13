b3a13aa31d7e3327a08866fda2d30921b1ea8fd1
渡辺のぞみ <@10.41.177.117>
2025-03-13 22:19:06 +0900

    Javaオブジェクト指向・カプセル化（演習②③完了）

--- src/capsule/Capsule.java
+++ src/capsule/Capsule.java
@@ -4,7 +4,10 @@ public class Capsule {
 
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
-
+		
+		Person taro = new Person("山田太郎",20);
+		System.out.println(taro.name);
+		System.out.println(taro.age);
+		}
 	}
 
-}

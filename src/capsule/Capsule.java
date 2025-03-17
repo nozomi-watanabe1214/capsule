106fa27f4491962aafa261767474e41dc2fa83b7
渡辺のぞみ <@10.18.238.186>
2025-03-17 21:27:47 +0900

    オブジェクト指向・カプセル化（演習⑤完了）

--- src/capsule/Capsule.java
+++ src/capsule/Capsule.java
@@ -8,9 +8,11 @@ public class Capsule {
 		Person taro = new Person("山田太郎",20);
 		System.out.println(taro.getName());
 		
-		taro.setName("花子");
+		System.out.println(taro.getAge());
 		
-		System.out.println(taro.getName());
+		taro.setAge(30);
+		System.out.println(taro.getAge());
 		}
 	}
+
 
